7c9cd81982e44a1079db793fe0567682e070aa0f
SnowHana <wujindaniel1011@gmail.com>
2024-07-02 21:24:48 +0900

    Added explanation which explain views or models?

--- backend/hiremonkey/base/urls.py
+++ backend/hiremonkey/base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("zoo/", views.zoo, name="zoo"),
+    path("ape/<str:pk>/", views.ape, name="ape"),
 ]
--- backend/hiremonkey/base/views.py
+++ backend/hiremonkey/base/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 
 
+# For testing. changer it alter
+apes = [{"id": 1, "name": "azir"}, {"id": 2, "name": "jax"}, {"id": 3, "name": "kaisa"}]
+
+
 def home(request):
-    return render(request, "base/home.html")
+    context = {"apes": apes}
+    return render(request, "base/home.html", context)
 
 
 def zoo(request):
     return render(request, "base/zoo.html")
+
+
+def ape(request, pk):
+    ape = None
+    for i in apes:
+        if i["id"] == int(pk):
+            ape = i
+    context = {"ape": ape}
+    return render(request, "base/ape.html", context)
