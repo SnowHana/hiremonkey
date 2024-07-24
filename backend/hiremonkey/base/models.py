5ef3a706eafb9ed060acdb63bae3ce398145a315
SnowHana <wujindaniel1011@gmail.com>
2024-07-24 11:50:28 +0900

    Composite pattern for Profile is done. Make a create profile form, once user is logged in

--- backend/hiremonkey/base/models.py
+++ backend/hiremonkey/base/models.py
@@ -24,21 +24,64 @@ class Skill(models.Model):
 class Profile(models.Model):
     JOB_SEEKER = "JS"
     RECRUITER = "RE"
-    # BOTH = "BO"
 
     PROFILE_CHOICES = [
         (JOB_SEEKER, "Job Seeker"),
         (RECRUITER, "Recruiter"),
-        # (BOTH, "Both"),
     ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profiles")
     profile_type = models.CharField(max_length=2, choices=PROFILE_CHOICES)
     bio = models.TextField(blank=True, null=True)
-    
-    # Highest level of education
-    edu_level = models.CharField(max_length=2, choices=)
-    academics = models.TextField(blank=True, null=True)
-    skills = models.ManyToManyField(Skill, related_name="profiles")
 
     def __str__(self):
         return f"{self.user.username} - {self.profile_type}"
+
+    class Meta:
+        abstract = True  # Mark this model as abstract
+
+
+class JobSeeker(Profile):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="job_seeker_profiles"
+    )
+    academics = models.TextField(blank=True, null=True)
+    skills = models.ManyToManyField("Skill", related_name="profiles")
+
+    def save(self, *args, **kwargs):
+        self.profile_type = Profile.JOB_SEEKER
+        super().save(*args, **kwargs)
+
+
+class Recruiter(Profile):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recruiter_profiles"
+    )
+    company = models.TextField(blank=False, null=False)
+
+    def save(self, *args, **kwargs):
+        self.profile_type = Profile.RECRUITER
+        super().save(*args, **kwargs)
+
+
+# class Profile(models.Model):
+#     JOB_SEEKER = "JS"
+#     RECRUITER = "RE"
+#     # BOTH = "BO"
+
+#     PROFILE_CHOICES = [
+#         (JOB_SEEKER, "Job Seeker"),
+#         (RECRUITER, "Recruiter"),
+#         # (BOTH, "Both"),
+#     ]
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profiles")
+#     profile_type = models.CharField(max_length=2, choices=PROFILE_CHOICES)
+#     bio = models.TextField(blank=True, null=True)
+
+#     # Highest level of education
+#     edu_level = models.CharField(max_length=2, choices=)
+#     academics = models.TextField(blank=True, null=True)
+#     skills = models.ManyToManyField(Skill, related_name="profiles")
+
+#     def __str__(self):
+#         return f"{self.user.username} - {self.profile_type}"
