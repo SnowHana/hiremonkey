da652d059f2a247f2ee8b007852bd6821de143ea
SnowHana <wujindaniel1011@gmail.com>
2024-07-24 11:30:38 +0900

    working on Profile

--- backend/hiremonkey/base/models.py
+++ backend/hiremonkey/base/models.py
@@ -14,29 +14,31 @@ from django.contrib.auth.models import User
 #         return self.name
 
 
-
 class Skill(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
-    
+
 
 class Profile(models.Model):
     JOB_SEEKER = "JS"
     RECRUITER = "RE"
-    BOTH = "BO"
+    # BOTH = "BO"
 
     PROFILE_CHOICES = [
         (JOB_SEEKER, "Job Seeker"),
         (RECRUITER, "Recruiter"),
-        (BOTH, "Both"),
+        # (BOTH, "Both"),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="profiles")
     profile_type = models.CharField(max_length=2, choices=PROFILE_CHOICES)
     bio = models.TextField(blank=True, null=True)
-    skills = models.ManyToManyField(Skill, related_name='profiles')
+    
+    # Highest level of education
+    edu_level = models.CharField(max_length=2, choices=)
+    academics = models.TextField(blank=True, null=True)
+    skills = models.ManyToManyField(Skill, related_name="profiles")
 
     def __str__(self):
         return f"{self.user.username} - {self.profile_type}"
-    
