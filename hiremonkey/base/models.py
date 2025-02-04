ac8a61c07e3434a2774190a855dd5a9bdbb362a4
Nautilus-Ubuntu <wujindaniel1011@gmail.com>
2025-02-04 16:03:51 +0900

    Updated user status enum to chocies. Should give error now so fix html and views.py.

--- hiremonkey/base/models.py
+++ hiremonkey/base/models.py
@@ -8,7 +8,7 @@ from django.db.models.signals import pre_save, post_save
 from .utils import slugify_instance_title
 
 
-class UserStatusEnum(Enum):
+class UserStatusEnum(models.TextChoices):
     """User Status Enum class
 
     Args:
@@ -41,12 +41,9 @@ class UserStatusEnum(Enum):
     def to_human_readable(cls, enum_value):
         """
         Convert enum value to human-readable name.
-        ie. 'R' -> 'Recruiter' , 'J' -> 'JobSeeker'
+        e.g. 'R' -> 'Recruiter', 'J' -> 'JobSeeker'
         """
-        for tag in cls:
-            if tag.value[0] == enum_value:
-                return tag.value[1]
-        raise ValueError(f"Invalid enum value: {enum_value}")
+        return dict(cls.choices).get(enum_value, f"Invalid enum value: {enum_value}")
 
 
 class Profile(models.Model):
@@ -59,25 +56,12 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     slug = models.SlugField(max_length=200, blank=True, null=True, unique=True)
-    # profile_type = models.CharField(max_length=2, choices=PROFILE_CHOICES)
-    title = models.CharField(max_length=200, default="default profile")
     bio = models.TextField(blank=True, null=True)
-
-    skills = models.ManyToManyField(
-        "Skill", related_name="%(class)s_profiles", blank=True
-    )
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    # Salary Range Fields
-    min_salary = models.FloatField(blank=True, null=True, default=0)
-    max_salary = models.FloatField(blank=True, null=True, default=0)
-
-    def clean(self):
-        # Ensure min_salary is less than or equal to max_salary
-        if self.min_salary and self.max_salary and self.min_salary > self.max_salary:
-            raise ValidationError(
-                "Minimum salary cannot be greater than maximum salary."
-            )
+    # def clean(self):
+    #     # Ensure min_salary is less than or equal to max_salary
+    #     pass
 
     def save(self, *args, **kwargs):
         # Call the clean method to enforce validation
@@ -214,23 +198,24 @@ class UserSession(models.Model):
 
     user_status = models.CharField(
         max_length=1,
-        choices=[(tag.value[0], tag.value[1]) for tag in UserStatusEnum],
-        default=UserStatusEnum.JOBSEEKER.value[0],
+        choices=UserStatusEnum.choices,
+        default=UserStatusEnum.JOBSEEKER,
     )
 
     activated_profile_id = models.PositiveIntegerField(null=True, blank=True)
 
     def is_jobseeker(self):
-        return self.user_status == UserStatusEnum.JOBSEEKER.value[0]
+        return self.user_status == UserStatusEnum.JOBSEEKER
 
     def is_recruiter(self):
-        return self.user_status == UserStatusEnum.RECRUITER.value[0]
+        return self.user_status == UserStatusEnum.RECRUITER
 
     def get_user_status(self):
-        if self.is_jobseeker():
-            return UserStatusEnum.JOBSEEKER.value[1]
-        else:
-            return UserStatusEnum.RECRUITER.value[1]
+        # if self.is_jobseeker():
+        #     return UserStatusEnum.JOBSEEKER.value[1]
+        # else:
+        #     return UserStatusEnum.RECRUITER.value[1]
+        return UserStatusEnum.to_human_readable(self.user_status)
 
     def get_activated_profile(self):
         """Get currently activated profile of User
@@ -264,6 +249,7 @@ class UserSession(models.Model):
 
     def set_activated_profile(self, profile_id):
         self.activated_profile_id = profile_id
+        self.save()
 
     def get_all_job_profiles(self):
         try:
@@ -280,16 +266,17 @@ class UserSession(models.Model):
             return None
 
     def __str__(self):
-        return f"{self.user} - {self.user_status} : Actiaved {self.get_activated_profile()}"
+        return f"{self.user} - {self.get_user_status()} : Actiaved {self.get_activated_profile()}"
 
 
 # Signals
+pre_save.connect(profile_pre_save, sender=Profile)
 pre_save.connect(profile_pre_save, sender=Recruiter)
 pre_save.connect(profile_pre_save, sender=JobSeeker)
 
 post_save.connect(profile_post_save, sender=JobSeeker)
 post_save.connect(profile_post_save, sender=Recruiter)
-
+post_save.connect(profile_post_save, sender=Profile)
 
 # # Simple Profile fn
 # def get_user_status(user):
